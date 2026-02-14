011725455bd0ca14761331ba4556505cf8e8a4a9
devsaad149 <sasaad149@gmail.com>
2026-02-14 22:14:11 +0500

    Fix: Switch to HashRouter to resolve GitHub Pages 404 on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ReactLenis } from '@studio-freight/react-lenis';
 import ThreeScene from './components/ThreeScene';
 import Home from './components/Home';
@@ -11,7 +11,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/Portfolio">
+    <Router>
       <ReactLenis root>
         <div className="App">
           <CustomCursor />
